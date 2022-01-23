54f7a2efcbac826acbfe23e9479e81af7f22393b
Alvaro Domingues <alvarodomingues@ice.ufjf.br>
2022-01-23 19:39:48 -0300

    Adiciona passo a Cena e Sprite

--- js/Cena.js
+++ js/Cena.js
@@ -17,4 +17,9 @@ export default class Cena{
     adicionar(sprite){
         this.sprites.push(sprite);
     }
+    passo(dt){
+        for (const sprite of this.sprites) {
+            sprite.passo(dt);
+        }
+    }
 }
