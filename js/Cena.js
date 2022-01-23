4912023f6af9830fcc1d96ba198050c5530c09e1
Alvaro Domingues <alvarodomingues@ice.ufjf.br>
2022-01-23 20:02:08 -0300

    Adiciona controle de play e pause

--- js/Cena.js
+++ js/Cena.js
@@ -6,6 +6,7 @@ export default class Cena{
         this.sprites = [];
         this.t0 = 0;
         this.dt = 0;
+        this.idAnim = null;
     }
     desenhar(){
         this.ctx.fillStyle = "grey";
@@ -32,6 +33,20 @@ export default class Cena{
         this.dt = (t-this.t0)/ 1000;
         this.passo(this.dt);
         this.desenhar();
+        this.iniciar();
         this.t0 = t;
+    }
+
+    iniciar(){
+        this.idAnim = requestAnimationFrame(
+            (t)=>{this.quadro(t); }
+        );
+
+    }
+
+    parar(){
+        cancelAnimationFrame(this.idAnim);
+        this.t0 = null;
+        this.dt = 0;
     }
 }
