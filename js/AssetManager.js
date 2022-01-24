067b55c1ac0a7ee4c3f72395a9ea97c540187aac
Alvaro Domingues <alvarodomingues@ice.ufjf.br>
2022-01-23 21:39:13 -0300

    Adiciona metodo acabou

--- js/AssetManager.js
+++ js/AssetManager.js
@@ -7,8 +7,13 @@ export default class AssetManager{
 
     carregaImagem(chave,source){
         const img1 = new Image();
-        img1.src = "assets/knight.png";
+        img1.addEventListener("load",function(){
+            console.log(`Imagem ${this.carregadas}/${this.aCarregar} carregada!`);
+            this.carregadas++;
+        });
+        img1.src = source;
         this.imagens.set(chave, img1);
+        this.aCarregar++;
     }
 
     img(chave){
@@ -20,5 +25,9 @@ export default class AssetManager{
             return `${((this.carregadas / this.aCarregar)*100).toFixed(2)}%`
         }
         return "Nada a carregar";
+    }
+
+    acabou(){
+        return this.carregadas === this.aCarregar;
     }
 }
